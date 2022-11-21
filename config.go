d02430a49e7765c11ad089b06cbe97c154610bf0
JoshuaDoes <joshuadoespersonalstuff@gmail.com>
2022-11-21 04:03:55 -0500

    config: Remove logger, fix things

--- config.go
+++ config.go
@@ -6,14 +6,7 @@ import (
 
 	"github.com/Clinet/clinet_features"
 	"github.com/JoshuaDoes/json"
-	"github.com/JoshuaDoes/logger"
 )
-
-var Log *logger.Logger
-
-func init() {
-	Log = logger.NewLogger("config", 2)
-}
 
 type ConfigType int
 const (
@@ -23,7 +16,7 @@ const (
 )
 
 type Config struct {
-	Features     []*features.Feature `json:"features"`
+	Features     []features.Feature `json:"features"`
 
 	path string //The path to the configuration file
 }
@@ -31,46 +24,39 @@ type Config struct {
 //NewConfig returns a new configuration struct
 func NewConfig() *Config {
 	return &Config{
-		Features: make([]*features.Feature, 0),
+		Features: make([]features.Feature, 0),
 	}
 }
 
 //LoadConfig creates a new configuration struct with the values in the specified configuration file
-func LoadConfig(path string, cfgType ConfigType) (cfg *Config, err error) {
-	Log.Trace("--- loadConfig(", path, ", ", cfgType, ") ---")
-
-	cfg = &Config{path: path}
+func LoadConfig(path string, cfgType ConfigType) (*Config, error) {
+	cfg := &Config{path: path}
 
 	switch cfgType {
 	case ConfigTypeJSON:
 		configJSON, err := ioutil.ReadFile(path)
 		if err != nil {
-			Log.Error("Error reading configuration file:", err)
 			return nil, err
 		}
 
 		err = json.Unmarshal(configJSON, cfg)
+		if err != nil {
+			return nil, err
+		}
 	default:
-		Log.Error("Unknown configuration type:", cfgType)
 		return nil, errors.New("bot: config: unknown configuration type")
 	}
 
-	return
+	return cfg, nil
 }
 
-func SaveConfig(cfg *Config, path string, cfgType ConfigType) (err error) {
-	Log.Trace("--- saveConfig(", path, ", ", cfgType, ") ---")
-
+func SaveConfig(cfg *Config, path string, cfgType ConfigType) error {
 	configJSON, err := json.Marshal(cfg, true)
 	if err != nil {
-		Log.Error("Error generating config JSON:", err)
 		return err
 	}
 
 	err = ioutil.WriteFile(path, configJSON, 0644)
-	if err != nil {
-		Log.Error("Error saving config JSON to path:", err)
-	}
 	return err
 }
 
